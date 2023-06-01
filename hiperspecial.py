1c2c89492c97cc4b1671e6c89deaf767a3d75e91
H4ns3l <hansel.hir@gmail.com>
2023-06-01 02:28:36 -0500

    delete file redundant

--- hiperspecial.py
+++
@@ -1,6 +0,0 @@
-from ansible.plugins.callback import CallbackBase
-from art import *
-
-class CallbackModule(CallbackBase):
-    def v2_playbook_on_stats(self, stats):
-        print(text2art("HIPER", font="block"))
